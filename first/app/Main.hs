938f542ded7a5cd84f3c70b4506862274bd5a5e6
maghrebi <sajad.faghfoor@gmail.com>
2022-12-08 00:39:07 +0330

    first

---
+++ first/app/Main.hs
@@ -0,0 +1,16 @@
+module Main (main) where
+import Data.Char
+
+import Lib
+
+main :: IO ()
+main = do
+    print $ isAlpha 'c'
+    print $ isDigit '4'
+    print $ toUpper 'a'
+    print $ toLower 'E'
+    print $ digitToInt '2'
+    print $ intToDigit 9
+    print $ intToDigit 12
+    print $ ord('A')
+    print $ chr(61)
